99a7f11da02405e6ad4eb2814236eed9244e7a0f
jackiossy <jackiossy@gmail.com>
2023-06-20 20:48:11 +0800

    add swiftui004 test.

---
+++ SwiftUI/SwiftUI004/SwiftUI004/SwiftUI004App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI004App.swift
+//  SwiftUI004
+//
+//  Created by Jackiossy on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI004App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
